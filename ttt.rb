03ddb96ac63dd3abd79c0d426a8146cd1f7b19f0
Kris Folds <wvkidd1979@gmail.com>
2018-05-31 17:45:27 -0400

    @#$%

--- ttt.rb
+++ ttt.rb
@@ -29,16 +29,18 @@ def winner(a1, a2, a3, a4, a5, a6, a7, a8, a9)
     return winner
 end
 
-def winning_combinations()
-	wc1 = [a1, a2, a3]
-	wc2 = [a4, a5, a6]
-	wc3	= [a7, a8, a9]
-	wc4 = [a1, a5, a9]
-	wc5	= [a3, a5, a7]
-	wc6	= [a1, a4, a7]
-	wc7	= [a2, a5, a8]
-	wc8	= [a3, a6, a9]
-end
+def comp_play(a1, a2, a3, a4, a5, a6, a7, a8, a9)
+	Hush = {1 => a1, 2 => a2, 3 => a3, 4 => a4, 5 => a5, 6 => a6, 7 => a7, 8 => a8, 9 => a9}
+	Arry = [a1, a2, a3, a4, a5, a6, a7, a8, a9]
+
+	wc1 = {1 => a1, 2 => a2, 3 => a3]
+	wc2 = [4 => a4, 5 => a5, 6 => a6]
+	wc3	= [7 => a7, 8 => a8, 9 => a9]
+	wc4 = [1 => a1, 5 => a5, 9 => a9]
+	wc5	= [3 => a3, 5 => a5, 7 => a7]
+	wc6	= [1 => a1, 4 => a4, 7 => a7]
+	wc7	= [2 => a2, 5 => a5, 8 => a8]
+	wc8	= [3 => a3, 6 => a6, 9 => a9]
 
 def corners()
 	corners = [a1, a3, a7, a9]

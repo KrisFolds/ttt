0ad3bba4b17f03197c2a57937fbd1a36af3f1554
Kris Folds <wvkidd1979@gmail.com>
2018-06-05 14:37:09 -0400

    1234

--- app.rb
+++ app.rb
@@ -1,13 +1,10 @@
 require 'sinatra'
 require_relative 'ttt.rb'
-
 enable :sessions
 
-   
 get '/' do
 	erb :home	
 end
-
 
 post '/play' do
 	session[:a1] = params[:a1]
@@ -33,71 +30,73 @@ get '/play' do
 	a7 = session[:a7]
 	a8 = session[:a8]
 	a9 = session[:a9]
-    play_type = session[:play_type]
-    puts play_type
-	if a1 == "O"
+	play_type = session[:play_type]
+	if    a1 == "O"
 	      a1 = "O"
-	elsif a1 == ""
-		  a1 = ""
-	else a1 = "X"
+	elsif a1 == "X"
+		  a1 = "X"
+	else a1 = """"
 	end
 	if a2 == "O"
 		a2 = "O"
-	elsif a2 == ""
-		a2 = ""
-	else a2 = "X"
+	elsif a2 == "X"
+		a2 = "X"
+	else a2 = """"
 	end
 	if a3 == "O"
 		a3 = "O"
-	elsif a3 == ""
-		a3 = ""
-	else a3 = "X"
+	elsif a3 == "X"
+		a3 = "X"
+	else a3 = """"
 	end
 	if a4 == "O"
 		a4 = "O"
-	elsif a4 == ""
-		a4 = ""
-	else a4 = "X"
+	elsif a4 == "X"
+		a4 = "X"
+	else a4 = """"
 	end
 	if a5 == "O"
 		a5 = "O"
-	elsif a5 == ""
-		a5 = ""
-	else a5 = "X"
+	elsif a5 == "X"
+		a5 = "X"
+	else a5 = """"
 	end
 	if a6 == "O"
 		a6 = "O"
-	elsif a6 == ""
-		a6 = ""
-	else a6 = "X"
+	elsif a6 == "X"
+		a6 = "X"
+	else a6 = """"
 	end
 	if a7 == "O"
 		a7 = "O"
-	elsif a7 == ""
-		a7 = ""
-	else a7 = "X"
+	elsif a7 == "X"
+		a7 = "X"
+	else a7 = """"
 	end
 	if a8 == "O"
 		a8 = "O"
-	elsif a8 == ""
-		a8 = ""
-	else a8 = "X"
+	elsif a8 == "X"
+		a8 = "X"
+	else a8 = """"
 	end
 	if a9 == "O"
 		a9 = "O"
-	elsif a9 == ""
-		a9 = ""
-	else a9 = "X"
+	elsif a9 == "X"
+		a9 = "X"
+	else a9 = """"
 	end
-	if play_type == "easy"
-		play_type = "easy"
-	elsif play_type == "hard"
-		play_type = "hard" 
+	if play_type == "wimp"
+		play_type = "wimp"
+	elsif play_type == "warrior"
+		play_type = "warrior" 
 	else play_type == """"
 	end	 		
 	if play_type == "wimp"
 		random = plays_random(a1, a2, a3, a4, a5, a6, a7, a8, a9)
-	end
+	else play_type == "warrior"
+		random = comp_play(a1, a2, a3, a4, a5, a6, a7, a8, a9)
+	end	
+	puts "random is #{random}"
 	if random == "a1"
 		a1 = "O"
 	elsif random == "a2"
@@ -116,9 +115,6 @@ get '/play' do
 		a8 = "O"
 	elsif random == "a9"
 		a9 = "O"
-	end
-	if play_type == "warrior"
-		random = comp_play(a1, a2, a3, a4, a5, a6, a7, a8, a9)	
 	end
 	erb :play, :locals => {:a1 => a1, :a2 => a2, :a3 => a3, :a4 => a4, :a5 => a5, :a6 => a6, :a7 => a7, :a8 => a8, :a9 => a9, :play_type => play_type}
 end
--- ttt.rb
+++ ttt.rb
@@ -1,33 +1,39 @@
-def plays_random(a1,a2,a3,a4,a5,a6,a7,a8,a9) 
+def plays_random(a1,a2,a3,a4,a5,a6,a7,a8,a9)
 	all_hash = {1 => a1, 2 => a2, 3 => a3, 4 => a4, 5 => a5, 6 => a6, 7 => a7, 8 => a8, 9 => a9}
+	puts "all_hash  is #{all_hash}"
 	all_squares = [1,2,3,4,5,6,7,8,9]
+	puts "all_squares is #{all_squares}"
 	used_squares = [] 
 	(1..9).each do |i|
 		if all_hash[i] != """" 
 			used_squares.push(i)
 		end
 	end
-	
+	puts "used_squares  is #{used_squares}"
 	available_squares = all_squares - used_squares
+	puts "available_squares pick  is #{available_squares}"	
 	random_pick = available_squares.shuffle.pop.to_s 
-	
+	puts "random pick  is #{random_pick}"
 	a = "a"
 	out = a + random_pick 
+	puts "out is #{out}"
 	return out
 end
 
 def winner(a1, a2, a3, a4, a5, a6, a7, a8, a9)
-   if a1 == "X" && a1 == a2 && a2 == a3 || a4 == "X" && a4 == a5 && a5 == a6 || a7 == "X" && a7 == a8 && a8 == a9 || a1 == "X" && a1 == "X" && a1 == a4 && a4 == a7 || a2 == "X" && a2 == a5 && a5 == a8 || a3 == "X" && a3 == a6 && a6 == a9|| a1 == "X" && a1 == a5 && a5 == a9 || a3  == "X" && a3 == a5 && a5 == a7
+   if a1 == "X" && a1 == a2 && a2 == a3 || a4 == "X" && a4 == a5 && a5 == a6 || a7 == "X" && a7 == a8 && a8 == a9 || a1 == "X" && a1 == "X" && a1 == a4 && a4 == a7 || a2 == "X" && a2 == a5 && a5 == a8 || a3 == "X" && a3 == a6 && a6 == a9|| a1 == "X" && a1 == a5 && a5 == a9 || a3  == "X" &&a3 == a5 && a5 == a7
         winner = "X"
    elsif a1 == "O" && a1 == a2 && a2 == a3 || a4 == "O" && a4 == a5 && a5 == a6 || a7 == "O" && a7 == a8 && a8 == a9 || a1 == "O" && a1 == "O" && a1 == a4 && a4 == a7 || a2 == "O" && a2 == a5 && a5 == a8 || a3 == "O" && a3 == a6 && a6 == a9|| a1 == "O" && a1 == a5 && a5 == a9 || a3  == "O" &&a3 == a5 && a5 == a7
         winner = "O"
    end
+    puts winner
     return winner
 end
 
+
 def comp_play(a1, a2, a3, a4, a5, a6, a7, a8, a9)
-	Hash == {1 => a1, 2 => a2, 3 => a3, 4 => a4, 5 => a5, 6 => a6, 7 => a7, 8 => a8, 9 => a9}
-	Array == [a1, a2, a3, a4, a5, a6, a7, a8, a9]
+	c_hash = {1 => a1, 2 => a2, 3 => a3, 4 => a4, 5 => a5, 6 => a6, 7 => a7, 8 => a8, 9 => a9}
+	all_values_array = [a1, a2, a3, a4, a5, a6, a7, a8, a9]
 
 	wc1 = [1 => a1, 2 => a2, 3 => a3]
 	wc2 = [4 => a4, 5 => a5, 6 => a6]
@@ -53,9 +59,9 @@ def comp_play(a1, a2, a3, a4, a5, a6, a7, a8, a9)
 	winning_Array = [wc1, wc2, wc3, wc4, wc5, wc6, wc7, wc8]
 	(0..8).each do |i|
 		bell = winning_Array[i]
-		x_dig = bell.select{|key, value| value == "X"}
-		o_dig = bell.select{|key, value| value == "O"}
-		na_dig = bell.select{|key, value| value == ""}
+		x_dig = bell.select {|key, value| value == "X"}
+		o_dig = bell.select {|key, value| value == "O"}
+		na_dig = bell.select {|key, value| value == ""}
 		if x_dig.size == 2
 			if o_dig.size < 1
 				$comp_pick = na_dig.key("")
@@ -64,9 +70,9 @@ def comp_play(a1, a2, a3, a4, a5, a6, a7, a8, a9)
 	end
 	(0..8).each do |i|
 		bell = winning_Array[i]
-		x_dig = bell.select{|key, value| value == "X"}
-		o_dig = bell.select{|key, value| value == "O"}
-		na_dig = bell.select{|key, value| value == ""}
+		x_dig = bell.select {|key, value| value == "X"}
+		o_dig = bell.select {|key, value| value == "O"}
+		na_dig = bell.select {|key, value| value == ""}
 		if o_dig.size == 2
 			if x_dig.size < 1
 				$comp_pick = na_dig.key("")
@@ -74,8 +80,7 @@ def comp_play(a1, a2, a3, a4, a5, a6, a7, a8, a9)
 		end
 	end
 	
-	g = "g"
-	over = g << $comp_pick.to_s
+	over = "g" << $comp_pick.to_s
 	return over
 end
 
